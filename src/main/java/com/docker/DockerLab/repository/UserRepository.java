3444437f87a6e52dc6c98b6502c7bf46696a036c
gertrude654 <gertrude.uzimana@amalitechtraining.org>
2024-09-18 12:19:11 +0200

    added docker compose and fix bugs

--- src/main/java/com/docker/DockerLab/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.docker.DockerLab.repository;
-//
-//import com.docker.DockerLab.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
